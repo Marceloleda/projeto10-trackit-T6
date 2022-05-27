25938585253b43f1e6f96eb0469099cd4989076c
Marceloleda <marcelo.leda@icomp.ufam.edu.br>
2022-05-27 09:40:36 -0400

    feito a tela de login e de cadastro, faltando fazer o post nas duas

--- src/Components/TelaCadastro.js
+++ src/Components/TelaCadastro.js
@@ -1,10 +1,119 @@
-import styled from 'styled-components'
+import React from 'react'
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from "axios";
+
+import logo from "../Assets/img/Group 8.png"
+
+
+
 export default function TelaCadastro(){
+    const [cadastro, setCadastro] = useState({
+        email:"",
+        senha:"",
+        nome:"",
+        foto:""
+    });
+    useEffect(() => {
+        const URL = `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up`;
+        const promise = axios.post(URL, {
+            
+        })
+    }, [])
+    console.log(cadastro)
     return(
-        <>
-            <h1>
-                tela cadastro
-            </h1>
+        <> 
+            <Imagem>
+                <img src={logo}/>
+            </Imagem>
+            <Login>
+                <Conteiner>
+                    <form>
+                        <CampoInfo id="email" type="email" placeholder="email" value={cadastro.email} onChange={
+                            (e) => setCadastro({...cadastro,
+                                email: e.target.value
+                            })
+                            } required />
+                        <CampoInfo id="senha" type="password" placeholder="senha" value={cadastro.senha} onChange={
+                            (e) => setCadastro({...cadastro,
+                                senha: e.target.value
+                            })
+                            } required />
+                         <CampoInfo id="nome" type="text" placeholder="nome" value={cadastro.nome} onChange={
+                            (e) => setCadastro({...cadastro,
+                                nome: e.target.value
+                            })
+                            } required />
+                         <CampoInfo id="foto" type="url" placeholder="foto" value={cadastro.foto} onChange={
+                            (e) => setCadastro({...cadastro,
+                                foto: e.target.value
+                            })
+                            } required />
+                        <Botao type="submit" >Cadastrar</Botao>
+                    </form>
+                </Conteiner>
+            </Login>
+            <Cadastro>
+                <Link to={`/`}>
+                    <h2>Já tem uma conta? Faça login!</h2>
+                </Link>
+            </Cadastro>
         </>
     );
 }
+
+const Imagem = styled.div`
+    display:flex;
+    justify-content:center;
+    margin-top: 68px;
+    img{
+        width: 180px;
+        height: 178px;
+    }
+
+`;
+const Conteiner = styled.div`
+    margin-left:36px;
+`;
+const Login = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+const Cadastro = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 25px;
+    h2{
+        color: #52B6FF;
+    }
+`;
+const CampoInfo = styled.input`
+    width: 303px;
+    height: 45px;
+    margin-bottom: 6px;
+    background: #FFFFFF;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    padding: 10px;
+    box-sizing: border-box;
+
+`;
+const Botao = styled.button`
+    width: 303px;
+    height: 45px;
+    background: #52B6FF;
+    border-radius: 4.63636px;
+    border:none;
+    cursor: pointer;
+
+    font-family: 'Lexend Deca';
+    font-size: 20.976px;
+    line-height: 26px;
+    text-align: center;
+
+    color: #FFFFFF;
+    &:hover {
+	    background: #0864a5;
+    }
+`;
--- src/Components/TelaLogin.js
+++ src/Components/TelaLogin.js
@@ -1,18 +1,39 @@
 import styled from 'styled-components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios";
+
 import logo from "../Assets/img/Group 8.png"
 
 
 
 export default function TelaLogin(){
+    const [login, setLogin] = useState({
+        email:"",
+        senha:""
+    });
+    console.log(login)
     return(
         <> 
             <Imagem>
                 <img src={logo}/>
             </Imagem>
             <Login>
-                <h1>login</h1>
+                <Conteiner>
+                    <form>
+                        <CampoInfo id="email" type="email" placeholder="email" value={login.email} onChange={
+                            (e) => setLogin({...login,
+                                email: e.target.value
+                            })
+                            } required />
+                        <CampoInfo id="senha" type="password" placeholder="senha" value={login.senha} onChange={
+                            (e) => setLogin({...login,
+                                senha: e.target.value
+                            })
+                            } required />
+                        <Botao type="submit" >Entrar</Botao>
+                    </form>
+                </Conteiner>
             </Login>
             <Cadastro>
                 <Link to={`/cadastro`}>
@@ -33,15 +54,47 @@ const Imagem = styled.div`
     }
 
 `;
+const Conteiner = styled.div`
+    margin-left:36px;
+`;
 const Login = styled.div`
+    display: flex;
+    justify-content: center;
     height:160px;
-    background-color: blue;
 `;
 const Cadastro = styled.div`
     display: flex;
     justify-content: center;
     margin-top: 25px;
     h2{
-        text-decoration:none;
+        color: #52B6FF;
+    }
+`;
+const CampoInfo = styled.input`
+    width: 303px;
+    height: 45px;
+    margin-bottom: 6px;
+    background: #FFFFFF;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    padding: 10px;
+    box-sizing: border-box;
+`;
+const Botao = styled.button`
+    width: 303px;
+    height: 45px;
+    background: #52B6FF;
+    border-radius: 4.63636px;
+    border:none;
+    cursor: pointer;
+
+    font-family: 'Lexend Deca';
+    font-size: 20.976px;
+    line-height: 26px;
+    text-align: center;
+
+    color: #FFFFFF;
+    &:hover {
+	    background: #0864a5;
     }
 `;
